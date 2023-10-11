ba07adb9b03067888ed57e709adf3781fb6a9eae
AJTimePyro <ajtimepyro@gmail.com>
2023-10-11 18:18:53 +0530

    improved navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,27 +1,85 @@
 import { FaSearch } from 'react-icons/fa';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { useState } from 'react';
+
 
 function NavBar() {
+    let menu_links = [
+        {
+            name : "Home",
+            link : "/"
+        },
+        {
+            name : "Songs",
+            link : "/"
+        },
+        {
+            name : "Artists",
+            link : "/"
+        },
+        {
+            name : "Artists",
+            link : "/"
+        }
+    ]
+
+    let [menu_btn, setmenu_btn] = useState(true);
+    let [close_btn, setclose_btn] = useState(false);
+
+    const toggleComponents = () => {
+        setmenu_btn(!menu_btn);
+        setclose_btn(!close_btn);
+    };
+
+    const MenuBtn = () => {
+        return <AiOutlineMenu/>;
+    };
+    
+    const CloseBtn = () => {
+        return <AiOutlineClose/>
+    };
+      
+    
+
     return (
         <>
-            <nav className="flex justify-between items-center gap-6 bg-slate-900 p-4 sm:pl-10 sm:pr-10">
-                <div className="">
-                    <h2 className="text-slate-400 text-xl md:text-2xl font-bold">
-                        <span>M</span>usic
-                        <span>X</span>
-                    </h2>
+            <nav className="sm:flex justify-between md:justify-around items-center gap-1 md:gap-3 bg-slate-900 p-4 md:pl-10 md:pr-10">
+                <div className="flex justify-between items-center gap-11">
+                    <div>
+                        <h2 className="text-slate-400 text-lg md:text-2xl font-bold">
+                            <span>M</span>usic
+                            <span>X</span>
+                        </h2>
+                    </div>
+
+                    <div className="bg-white items-center rounded-3xl md:rounded-2xl h-10 w-auto flex">
+                        <input id="input-box" type="text" placeholder="Enter search query..."  className="ml-4 mr-2 border-none focus:outline-none"/>
+                        <FaSearch className="hover:cursor-pointer text-blue-500 mr-4"/>
+                    </div>
+
+                    <div className="text-white sm:hidden cursor-pointer text-3xl" onClick={toggleComponents}>
+                        {menu_btn && <MenuBtn />}
+                        {close_btn && <CloseBtn />}
+                    </div>
                 </div>
 
-                <div className="text-slate-400 text-xl md:text-2xl font-ubuntu flex items-center gap-1 md:gap-6">
-                    <a className="hover:rounded-2xl hover:bg-gray-300 hover:text-slate-700 p-1" href="#">Home</a>
-                    <a className="hover:rounded-2xl hover:bg-gray-300 hover:text-slate-700 p-1" href="#">Songs</a>
-                    <a className="hover:rounded-2xl hover:bg-gray-300 hover:text-slate-700 p-1" href="#">Artists</a>
-                    <a className="hover:rounded-2xl hover:bg-gray-300 hover:text-slate-700 p-1" href="#">Albums</a>
+
+                <div>
+                    <ul className={
+                        `text-slate-400 text-lg md:text-2xl font-ubuntu sm:flex sm:items-center gap-1 md:gap-3 transition-all duration-500 ease-in ${close_btn ? "opacity-100" : "hidden"}
+                        `}>
+                        {
+                            menu_links.map(
+                                (links) => (
+                                    <li>
+                                        <a className="block text-center focus:bg-gray-300 focus:text-slate-700 sm:hover:rounded-2xl sm:hover:bg-gray-300 sm:hover:text-slate-700" href={links.link}>{links.name}</a>
+                                    </li>
+                                )
+                            )
+                        }
+                    </ul>
                 </div>
 
-                <div className="bg-white items-center rounded-3xl md:rounded-2xl h-10 w-auto flex">
-                    <input id="input-box" type="text" placeholder="Enter Song, Artists, ... name"  className="ml-4 z-10 border-none focus:outline-none h-full bg-transparent"/>
-                    <FaSearch className="hover:cursor-pointer text-blue-500 mr-4"/>
-                </div>
             </nav>
         </>
     )
