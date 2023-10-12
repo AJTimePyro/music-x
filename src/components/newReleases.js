e73b59156d9d803143575ed502a09eef53198c57
AJTimePyro <ajtimepyro@gmail.com>
2023-10-12 23:40:46 +0530

    adding new release section

---
+++ src/components/newReleases.js
@@ -0,0 +1,17 @@
+function NewReleases() {
+    return (
+        <section>
+            <div className="p-4">
+                <h2 className="text-slate-100 font-bold text-3xl">
+                    New Releases
+                </h2>
+            </div>
+
+            <div>
+                
+            </div>
+        </section>
+    )
+}
+
+export default NewReleases;
