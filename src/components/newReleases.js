b7f46d236bcb0cbc7e0080f6535761b6fa61f125
AJTimePyro <ajtimepyro@gmail.com>
2023-10-14 17:52:10 +0530

    added scrolling left right button

--- src/components/newReleases.js
+++ src/components/newReleases.js
@@ -1,15 +1,32 @@
 import MusicCard from "./musicCard";
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
 function NewReleases() {
+
+    const slideLeft = () => {
+        let slider = document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        let slider = document.getElementById("slider");
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
+
     return (
         <section>
-            <div className="p-4">
+            <div className="p-4 flex justify-between">
                 <h2 className="text-slate-100 font-bold text-3xl">
                     New Releases
                 </h2>
+
+                <div className="flex text-slate-200">
+                    <MdChevronLeft size={50} onClick={slideLeft} className="cursor-pointer"/>
+                    <MdChevronRight size={50} onClick={slideRight} className="cursor-pointer"/>
+                </div>
             </div>
 
-            <div className="flex w-full h-full overflow-x-scroll scroll-smooth whitespace-nowrap">
+            <div id="slider" className="flex w-full h-full overflow-x-scroll scroll-smooth whitespace-nowrap">
                 <MusicCard/>
                 <MusicCard/>
                 <MusicCard/>
