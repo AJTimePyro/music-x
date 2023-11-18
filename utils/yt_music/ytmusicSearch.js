25f503930cf35d0a818e700ec5c5dba521bbb77b
AJTimePyro <ajtimepyro@gmail.com>
2023-11-18 19:20:34 +0530

    adding searching script

---
+++ utils/yt_music/ytmusicSearch.js
@@ -0,0 +1,128 @@
+const YTMusicBase = require("./ytmusicBase");
+
+class YTMusicSearch extends YTMusicBase {
+
+    constructor(query) {
+        super();
+        this.payloadWebClient["query"] = query;
+        this.payloadAndroidClient["params"] = "EgWKAQIIAWoQEAoQCRADEAQQBRAREBAQFQ%3D%3D";
+        this.searchResults = [];
+
+        let _searchData = async () => {
+            const res = await super.sendpostreq(
+                this.api_url + "youtubei/v1/search?key=" + this.key,
+                this.payloadWebClient
+            );
+
+            if ("error" in res) {
+                throw new Error("Unable to get search result...");
+            }
+            else {
+                this.chunkData = res;
+                _parseData();
+            }
+        };
+
+        let _parseData = () => {
+            const trueContent = this.chunkData["contents"]["tabbedSearchResultsRenderer"]["tabs"][0]["tabRenderer"]["content"]["sectionListRenderer"]["contents"][0]["musicShelfRenderer"]["contents"];
+
+            for (const index in trueContent) {
+                const musicContent = trueContent[index];
+                const mRLITR = musicContent["musicResponsiveListItemRenderer"];
+                const musicFlexCol = musicContent["flexColumns"];
+                const musicFlexCol1 = musicFlexCol[1];
+                const mRLITFCRruns = _getmRLITFCRruns(musicFlexCol1);
+
+                let tempObj = {};
+
+                // Thumbnail parsing
+                tempObj["thumbnail"] = _parseThumbnail(mRLITR);
+
+                // Music ID & Title parsing
+                const idTitle = _parseTitleAndID(musicFlexCol[0]);
+                tempObj["music_id"] = idTitle[0];
+                tempObj["title"] = idTitle[1];
+
+                // Artist Info Parsing
+                const artist = _getArtist(mRLITFCRruns[0]);
+                tempObj["artist_name"] = artist[0];
+                tempObj["artist_channel_id"] = artist[1];
+
+                // Album Info Parsing
+                const album = _getAlbum(mRLITFCRruns[2]);
+                tempObj["album_name"] = album[0];
+                tempObj["album_channel_id"] = album[1];
+
+                // Duration parsing
+                tempObj["duration"] = _parseDuration(mRLITFCRruns[4]);
+
+                tempObj["id"] = parseInt(index);
+
+                this.searchResults.push(tempObj);
+            }
+        };
+
+        let _parseThumbnail = (mRLITR) => {
+            const thumbnailList = mRLITR["musicThumbnailRenderer"]["thumbnail"]["thumbnails"];
+            return super.getBestThumbnail(thumbnailList);
+        };
+
+        let _parseTitleAndID = (musicFlexCol) => {
+            const mRLITFCRruns = _getmRLITFCRruns(musicFlexCol);
+            const mRLITFCRruns0 = mRLITFCRruns[0];
+            const title = mRLITFCRruns0["text"];
+
+            const watchEndPoint = mRLITFCRruns0["navigationEndpoint"]["watchEndpoint"];
+            const musicId = watchEndPoint["videoId"];
+
+            return [musicId, title];
+        };
+
+        let _getmRLITFCRruns = (musicFlexCol) => {
+            return musicFlexCol["musicResponsiveListItemFlexColumnRenderer"]["text"]["runs"];
+        };
+
+        let _getArtist = (mRLITFCRruns) => {
+            const artistName = mRLITFCRruns["text"];
+            const artistChannelId = _getNavBrEndpointBrId(mRLITFCRruns);
+
+            return [artistName, artistChannelId];
+        };
+
+        let _getNavBrEndpointBrId = (mRLITFCRruns) => {
+            return mRLITFCRruns["navigationEndpoint"]["browseEndpoint"]["browseId"];
+        };
+
+        let _getAlbum = (mRLITFCRruns) => {
+            const albumName = mRLITFCRruns["text"];
+            const albumChannelId = _getNavBrEndpointBrId(mRLITFCRruns);
+
+            return [albumName, albumChannelId];
+        };
+
+        let _parseDuration = (musicFlexCol) => {
+            const mRLITFCRruns = _getmRLITFCRruns(musicFlexCol);
+            const mRLITFCRrunsLast = mRLITFCRruns[mRLITFCRruns.length - 1];
+
+            return mRLITFCRrunsLast["text"];
+        };
+
+        this.extraction = async () => {
+            await _searchData();
+        }
+    };
+
+    async start() {
+        await this.extraction();
+    }
+}
+
+async function getYTMSearchResult(
+    query
+) {
+    let ytNewRe = new YTMusicSearch(query);
+    await ytNewRe.start();
+    return ytNewRe.songsList;
+}
+
+module.exports = getYTMSearchResult;
