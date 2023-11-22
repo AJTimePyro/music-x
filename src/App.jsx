fabdce4416dd4a269ee99feff59620c4457ea1b8
AJTimePyro <ajtimepyro@gmail.com>
2023-11-22 22:11:19 +0530

    added react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import NavBar from './components/pageComponent/navbar'
-import HomeBody from './components/pageComponent/homeContent'
 import MusicPlayer from './components/pageComponent/musicPlayer';
-import MusicPlayState from './context/musicPlayInfo/playContextProvider'
+import MusicPlayState from './context/musicPlayInfo/playContextProvider';
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
   return (
     <MusicPlayState>
       <NavBar/>
-      <HomeBody/>
+      <Outlet/>
       <MusicPlayer/>
     </MusicPlayState>
   );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
 import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomeBody from './components/pageComponent/homeContent';
+import './css/index.css';
+
+const router = createBrowserRouter(
+  [
+    {
+      path : '/',
+      element : <App/>,
+      children : [
+        {
+          path : '',
+          element : <HomeBody/>
+        },
+        {
+          path : '/search',
+          // element : 
+        }
+      ]
+    }
+  ]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
