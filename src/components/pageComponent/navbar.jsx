a00fc4f7cd8af140e0286bab58d3e2dcd9e2166b
AJTimePyro <ajtimepyro@gmail.com>
2023-12-19 14:02:40 +0530

    added website logo in navbar

--- src/components/pageComponent/navbar.jsx
+++ src/components/pageComponent/navbar.jsx
@@ -56,17 +56,14 @@ function NavBar() {
     };
 
     return (
-        <nav className="sm:flex justify-between md:justify-around items-center gap-1 md:gap-3 bg-slate-900 p-4 md:pl-10 md:pr-10 select-none">
-            <div className="flex justify-between items-center gap-11 mb-1 max-[472px]:gap-2">
+        <nav className="sm:flex justify-between md:justify-around items-center gap-1 md:gap-3 bg-slate-900 pt-4 pb-4 pr-2 pl-2 min-[420px]:pl-6 min-[420px]:pr-6 md:pl-8 md:pr-8 select-none">
+            <div className="flex justify-between items-center gap-11 max-lg:gap-6 max-sm:gap-1 mb-1">
                 <div>
-                    <h2 className="text-slate-400 text-lg md:text-2xl font-bold">
-                        <span>M</span>usic
-                        <span>X</span>
-                    </h2>
+                    <img src="/music-x-high-resolution-logo-transparent.png" className="h-8 max-lg:h-6 max-[420px]:h-5" alt="" draggable="false"/>
                 </div>
 
                 <div className="bg-white items-center rounded-3xl md:rounded-2xl h-10 w-auto flex">
-                    <input id="input-box" type="text" placeholder="Enter search query..."  className="ml-4 mr-2 border-none focus:outline-none max-[472px]:w-36" onKeyDown={handleKeyDown}/>
+                    <input id="input-box" type="text" placeholder="Enter search query..."  className="ml-4 mr-2 border-none focus:outline-none max-lg:w-44 max-sm:w-42 max-[420px]:w-32" onKeyDown={handleKeyDown}/>
                     <FaSearch className="hover:cursor-pointer text-blue-500 mr-4" onClick={searchTrigger}/>
                 </div>
 
@@ -80,7 +77,7 @@ function NavBar() {
             <div className={`transition-[height] duration-300 ease-in ${close_btn ? "max-sm:h-full" : "max-sm:h-0"}`}>
                 <ul
                     className={
-                        `text-slate-400 text-lg md:text-2xl font-ubuntu sm:flex sm:items-center gap-1 md:gap-3 transition-transform duration-300 ease-in-out ${!close_btn ? 'max-sm:translate-x-[-100%]' : ''}`
+                        `text-slate-400 text-lg md:text-2xl font-ubuntu sm:flex sm:items-center gap-3 max-lg:gap-1 transition-transform duration-300 ease-in-out ${!close_btn ? 'max-sm:translate-x-[-100%]' : ''}`
                     }>
                     {
                         menu_links.map(
